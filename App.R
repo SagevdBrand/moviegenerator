6754290805dc61bdcf2cb493e89fda95b12d3d26
Sofie vd Brand <64579032+SagevdBrand@users.noreply.github.com>
2021-01-18 11:27:51 +0100

    Connect app + fix bugs
    Connect app, chang .gitignore so that the deployment files remain on my pc only.

--- App.R
+++ App.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(tidyverse)
-source("Functions.R")
+#source("Functions.R")
 data <- read_csv("data/appdata.csv")
 genres <- read_rds("data/genres.Rds") 
 
@@ -58,7 +58,29 @@ server <- function(input, output) {
     
     
     input$actionbutton
-    isolate(find_movie(choices = input))
+    isolate(
+    if (input$choice1 == "Empty" & input$choice2 == "Empty") {
+      
+      print("Please provide at least 1 genre")
+      
+    } else if (input$choice1 == "Empty") {
+      
+      t_r <-  which(str_detect(data$genres, input$choice2))
+      as.character(data[floor(runif(1, min = 0, max = length(t_r))), 2])
+      
+    } else if (input$choice2 == "Empty") {
+      
+      t_r <-  which(str_detect(data$genres, input$choice1))
+      as.character(data[floor(runif(1, min = 0, max = length(t_r))), 2])
+      
+    } else {
+      
+      # First get the rownumbers of those movies that contain both genres
+      t_r <- intersect(which(str_detect(data$genres, input$choice1)), 
+                       which(str_detect(data$genres, input$choice2)))
+      # Get a random rownumber and return it's respective title
+      as.character(data[floor(runif(1, min = 0, max = length(t_r))), 2])
+    })
     
     
     })
--- Functions.R
+++ Functions.R
@@ -1,24 +1,24 @@
-find_movie <- function(choices){
+find_movie <- function(input){
     # count the empties
-  if (choices$choice1 == "Empty" & choices$choice2 == "Empty") {
+  if (input$choice1 == "Empty" & input$choice2 == "Empty") {
     
     print("Please provide at least 1 genre")
     
-    } else if (choices$choice1 == "Empty") {
-  
-      t_r <-  which(str_detect(data$genres, choices$choice2))
-      as.character(data[floor(runif(1, min = 0, max = length(t_r))), 2])
-      
-    } else if (choices$choice2 == "Empty") {
-      
-      t_r <-  which(str_detect(data$genres, choices$choice1))
-      as.character(data[floor(runif(1, min = 0, max = length(t_r))), 2])
-      
-      } else {
+  } else if (input$choice1 == "Empty") {
+    
+    t_r <-  which(str_detect(data$genres, input$choice2))
+    as.character(data[floor(runif(1, min = 0, max = length(t_r))), 2])
+    
+  } else if (input$choice2 == "Empty") {
+    
+    t_r <-  which(str_detect(data$genres, input$choice1))
+    as.character(data[floor(runif(1, min = 0, max = length(t_r))), 2])
+    
+  } else {
     
     # First get the rownumbers of those movies that contain both genres
-    t_r <- intersect(which(str_detect(data$genres, choices$choice1)), 
-                     which(str_detect(data$genres, choices$choice2)))
+    t_r <- intersect(which(str_detect(data$genres, input$choice1)), 
+                     which(str_detect(data$genres, input$choice2)))
     # Get a random rownumber and return it's respective title
     as.character(data[floor(runif(1, min = 0, max = length(t_r))), 2])
   }
